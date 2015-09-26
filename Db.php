29b535b183b221fab8f63f38e4ae40953a8983db
Christian Amaral <darthcas@gmail.com>
2015-09-26 13:48:57 -0300

    test db ok

---
+++ Db.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$config = parse_ini_file('config.ini', true);
+
+$db = (object) $config['db'];
+
+ORM::configure("mysql:host={$db->host};dbname={$db->name}");
+ORM::configure('username', $db->username);
+ORM::configure('password', $db->password);
+ORM::configure('error_mode', PDO::ERRMODE_WARNING);
+
+class Db {
+	
+	public function __get($name) {
+		return ORM::for_table($name);
+	}
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
-$config = parse_ini_file('config.ini', true);
+require 'Db.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-phpinfo();
+$config = parse_ini_file('config.ini', true);
+$klein = new \Klein\Klein();
+
+$klein->respond(function ($request, $response, $service, $app) use ($config) {
+  
+  $app->register('db', function() {
+
+    return new Db;
+
+  });
+
+});
+
+$klein->respond('GET', '/', function ($req, $res, $service, $app) {
+
+	$res->body(
+
+		$app->db->
+			test->where_equal('id', 1)->
+			find_one()->name
+
+	)->send();
+
+});
+
+$klein->dispatch();
